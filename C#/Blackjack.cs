6237db51e6a8452c56cfdc8d0009c5abc3a21347
Hasan <erkeshasan@gmail.com>
2024-10-24 07:51:39 +0300

    Update Blackjack.cs

--- C#/Blackjack.cs
+++ C#/Blackjack.cs
@@ -90,14 +90,17 @@ Random random = new Random();
 int[] DealerCardIDs = new int[22];
 int[] PlayerCardIDs = new int[22];
 
-for (int i = 0; i < DealerCardIDs.Length; i++)
+void ResetCards()
 {
-    DealerCardIDs[i] = -1;
-}
+    for (int i = 0; i < DealerCardIDs.Length; i++)
+    {
+        DealerCardIDs[i] = -1;
+    }
 
-for (int i = 0; i < PlayerCardIDs.Length; i++)
-{
-    PlayerCardIDs[i] = -1;
+    for (int i = 0; i < PlayerCardIDs.Length; i++)
+    {
+        PlayerCardIDs[i] = -1;
+    }
 }
 
 int TakeRandomCard()
@@ -166,173 +169,215 @@ for (int CardIndex = 0; CardIndex < 52; CardIndex++)
     Console.Write("\n");
 }
 
-bool dealt = false;
-bool hit = false;
-bool natural = false;
-bool stand = false;
-bool run = true;
+Console.Write("\nRules:\n * Ace: Ace cards (A) can either be a hard 1 or a soft 11 depending on the value of the hand.\n * Initial Deal: The dealer takes 2 random cards, one is shown and one is hidden, afterwards 2 cards are given to the player.\n * Player Input: The player may hit (h) to get a random card from the deck or stand (s) to begin comparing their cards against the dealer.\n * Player Bust: If the player's hand exceeds 21, the player busts and lose their bet.\n * Dealer Input: When the player stands, the dealer will take a random card from the deck until his hand adds up to a soft 17.\n * Dealer Bust: If the dealer's hand goes over 21, the dealer busts and the player wins.\n * Winning: Player wins if their hand total is higher than the dealer's hand total without exceeding 21.\n * Push: If the player's hand total equals the dealer's hand total, it results in a push and the bet is returned without any win or loss.\n * Natural Blackjack: If the player's hand is exactly 21 with only 2 cards, it is called a blackjack and they immediately win; Unless the dealer also has a blackjack in which case it is a push.");
 
-while (run)
+bool playing = true;
+
+Console.Write("\n\nTotal Money: $");
+double money = Convert.ToDouble(Console.ReadLine());
+
+while (playing)
 {
-    if (!dealt)
+    ResetCards();
+
+    bool InitialDeal = true;
+    bool hit = false;
+    bool stand = false;
+    bool run = true;
+
+    bool validBet = false;
+    double bet = 0;
+
+    Console.Write("\n\nMoney: $" + money);
+
+    while (!validBet)
+    {
+        Console.Write("\n\nNew Bet: $");
+        bet = Convert.ToDouble(Console.ReadLine());
+
+        if (bet < 0)
+        {
+            Console.WriteLine("Bet can't be less than 0.");
+        }
+        else if (money - bet < 0)
+        {
+            Console.WriteLine("Can't bet more money than you have.");
+        }
+        else
+        {
+            validBet = true;
+        }
+    }
+
+    while (run)
     {
         Console.Clear();
+        Console.Write("\nBet: $" + bet);
 
-        PlayerCardIDs[0] = TakeRandomCard();
-        PlayerCardIDs[1] = TakeRandomCard();
-
-        DealerCardIDs[0] = TakeRandomCard();
-        DealerCardIDs[1] = TakeRandomCard();
-    }
-
-    // Dealer Data
-
-    int DealerCardCount = IntArrayElementCount(DealerCardIDs);
-    int DealerHandValue = CalculateHighestSafeValue(DealerCardIDs);
-
-    // Player Data
-
-    int PlayerCardCount = IntArrayElementCount(PlayerCardIDs);
-    int PlayerHandValue = CalculateHighestSafeValue(PlayerCardIDs);
-
-    // Natural Blackjack
-
-    if (!dealt)
-    {
-        if (PlayerHandValue == 21)
+        if (InitialDeal) // TakeRandomCard()
         {
-            natural = true;
+            DealerCardIDs[0] = TakeRandomCard();
+            DealerCardIDs[1] = TakeRandomCard();
+
+            PlayerCardIDs[0] = TakeRandomCard();
+            PlayerCardIDs[1] = TakeRandomCard();
         }
 
-        dealt = true;
-    }
+        // Dealer Data
 
-    // Dealer Print
+        int DealerCardCount = IntArrayElementCount(DealerCardIDs);
+        int DealerHandValue = CalculateHighestSafeValue(DealerCardIDs);
 
-    Console.Write("Dealer:\n");
+        // Player Action
 
-    if (stand || natural)
-    {
-        for (int CardIndex = 0; CardIndex < DealerCardCount; CardIndex++)
+        if (hit)
         {
-            PrintCard(DealerCardIDs[CardIndex]);
+            Console.Write("\n\nHit");
+            hit = false;
+            PlayerCardIDs[IntArrayElementCount(PlayerCardIDs)] = TakeRandomCard();
+        }
+        else if (stand)
+        {
+            Console.Write("\n\nStand");
+        }
 
-            if (!(CardIndex == DealerCardCount - 1))
+        // Player Data
+
+        int PlayerCardCount = IntArrayElementCount(PlayerCardIDs);
+        int PlayerHandValue = CalculateHighestSafeValue(PlayerCardIDs);
+
+        // Dealer Print
+
+        Console.Write("\n\nDealer:\n");
+
+        if (stand)
+        {
+            for (int CardIndex = 0; CardIndex < DealerCardCount; CardIndex++)
+            {
+                PrintCard(DealerCardIDs[CardIndex]);
+
+                if (!(CardIndex == DealerCardCount - 1))
+                {
+                    Console.Write(" + ");
+                }
+            }
+
+            Console.Write(" = " + DealerHandValue);
+        }
+        else
+        {
+            PrintCard(DealerCardIDs[0]);
+
+            Console.Write(" + ?");
+        }
+
+        // Player Print
+
+        Console.Write("\n\nPlayer:\n");
+
+        for (int CardIndex = 0; CardIndex < PlayerCardCount; CardIndex++)
+        {
+            PrintCard(PlayerCardIDs[CardIndex]);
+
+            if (!(CardIndex == PlayerCardCount - 1))
             {
                 Console.Write(" + ");
             }
         }
 
-        Console.Write(" = " + DealerHandValue);
-    }
-    else
-    {
-        PrintCard(DealerCardIDs[0]);
+        Console.Write(" = " + PlayerHandValue);
 
-        Console.Write(" + ?");
-    }
+        // Player Stand
 
-    // Player Action
-
-    if (hit)
-    {
-        Console.Write("\n\nHit");
-        hit = false;
-        PlayerCardIDs[PlayerCardCount] = TakeRandomCard();
-    }
-    else if (stand)
-    {
-        Console.Write("\n\nStand");
-    }
-    else if (natural)
-    {
-        Console.Write("\n\nNatural");
-    }
-
-    Console.Write("\n\n");
-
-    // Player Print
-
-    Console.Write("Player:\n");
-
-    for (int CardIndex = 0; CardIndex < PlayerCardCount; CardIndex++)
-    {
-        PrintCard(PlayerCardIDs[CardIndex]);
-
-        if (!(CardIndex == PlayerCardCount - 1))
+        if (stand)
         {
-            Console.Write(" + ");
+            if (PlayerCardCount == 2 && PlayerHandValue == 21) // Natural Blackjack
+            {
+                if (DealerHandValue == 21)
+                {
+                    Console.Write("\n\nPush!\n\n");
+                    run = false;
+                }
+                else
+                {
+                    Console.Write("\n\nBlackjack!\n\n");
+                    money += bet * 1.5;
+                    run = false;
+                }
+            }
+            else // Normal
+            {
+                if (DealerHandValue < 17)
+                {
+                    Thread.Sleep(1000);
+                    DealerCardIDs[DealerCardCount] = TakeRandomCard();
+                }
+                else if (DealerHandValue > 21)
+                {
+                    Console.Write("\n\nDealer Bust!\n\n");
+                    money += bet;
+                    run = false;
+                }
+                else if (DealerHandValue == PlayerHandValue)
+                {
+                    Console.Write("\n\nPush!\n\n");
+                    run = false;
+                }
+                else if (DealerHandValue < PlayerHandValue)
+                {
+                    Console.Write("\n\nYou win!\n\n");
+                    money += bet;
+                    run = false;
+                }
+                else if (DealerHandValue > PlayerHandValue)
+                {
+                    Console.Write("\n\nYou lose!\n\n");
+                    money -= bet;
+                    run = false;
+                }
+            }
         }
-    }
 
-    Console.Write(" = " + PlayerHandValue);
+        // Player Bust
 
-    // Player Stand
-
-    if (stand)
-    {
-        DealerCardIDs[DealerCardCount] = TakeRandomCard();
-
-        int DealerValue = CalculateHighestSafeValue(DealerCardIDs);
-
-        if (DealerValue > 21)
+        if (PlayerHandValue > 21)
         {
-            Console.Write("\n\nDealer Bust!\n\n");
-            run = false;
-        } else if (DealerValue == PlayerHandValue)
-        {
-            Console.Write("\n\nPush!\n\n");
-            run = false;
-        }
-        else if (DealerValue >= 17 && DealerValue < PlayerHandValue)
-        {
-            Console.Write("\n\nwin\n\n");
+            Console.Write("\n\nPlayer Bust!\n\n");
+            money -= bet;
             run = false;
         }
-    }
 
-    // Player Bust
+        // Natural Blackjack Rule
 
-    if (PlayerHandValue > 21)
-    {
-        Console.Write("\n\nBust!\n\n");
-        run = false;
-    }
-
-    // Natural
-
-    if (natural)
-    {
-        if (DealerHandValue == 21)
+        if (InitialDeal)
         {
-            Console.Write("\n\nPush!\n\n");
-            run = false;
+            if (PlayerHandValue == 21)
+            {
+                stand = true;
+            }
+
+            InitialDeal = false;
         }
-        else
-        {
-            Console.Write("\n\nBlackjack!\n\n");
-            run = false;
-        }
-    }
 
-    // Input
+        // Input
 
-    if (!stand && run && !natural)
-    {
-        string Key = Console.ReadKey().KeyChar.ToString().ToLower();
-        Console.Clear();
+        if (!stand && run)
+        {
+            string Key = Console.ReadKey().KeyChar.ToString().ToLower();
 
-        if (Key == "h")
-        {
-            hit = true;
-        }
-        else if (Key == "s")
-        {
-            stand = true;
-        }
-        else if (Key == "q")
-        {
-            run = false;
+            if (Key == "h")
+            {
+                hit = true;
+            }
+            else if (Key == "s")
+            {
+                stand = true;
+            }
+            else if (Key == "q")
+            {
+                playing = false;
+                run = false;
+            }
         }
     }
 }
